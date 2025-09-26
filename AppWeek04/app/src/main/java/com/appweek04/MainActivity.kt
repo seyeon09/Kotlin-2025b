8aff03db013db580582947133c1ef3c919953851
seyeon09 <tpdus0919@dongyang.ac.kr0>
2025-09-26 15:04:22 +0900

    week04: Greeting App v0.2

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.appweek04
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -20,13 +21,17 @@ class MainActivity : AppCompatActivity() {
         buttonGreet.setOnClickListener(){
             val name = editTextName.text.toString().trim()
 
+            var greeting: String = ""
             if(name.isNotEmpty()){
-                textViewGreeting.text="안녕하세요 $(name)님 !"
-                textViewGreeting.visibility = View.VISIBLE
+                greeting = "안녕하세요 $(name)님 !"
+
             } else{
-                textViewGreeting.text="나의 이름은?"
-                textViewGreeting.visibility = View.VISIBLE
+                greeting = "나의 이름은?"
+
             }
+            textViewGreeting.text=greeting
+            textViewGreeting.visibility = View.VISIBLE
+            Log.d("KotlinWeek04App",greeting)
         }
     }
 }
