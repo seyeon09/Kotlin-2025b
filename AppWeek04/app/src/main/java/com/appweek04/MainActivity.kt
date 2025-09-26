0097e81af7f17f326f5711a5c2444d92b0f60e93
seyeon09 <tpdus0919@dongyang.ac.kr0>
2025-09-26 16:07:18 +0900

    week04: Greeting App v0.5

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.appweek04
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -14,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val buttonGreeting = findViewById<Button>(R.id.buttonGreeting)
 
+        buttonGreeting.setOnClickListener{
+            startActivity(Intent(this, GreetingActivity::class.java))
+        }
     }
 }
