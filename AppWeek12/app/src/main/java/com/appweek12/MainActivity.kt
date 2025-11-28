19ad2e4eb35e861e8271d657fe3f3ebd2df8ae2a
seyeon09 <tpdus0919@dongyang.ac.kr>
2025-11-28 14:48:23 +0900

    week12 : Counter App v0.4(Backend)

--- AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
+++ AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
@@ -26,8 +26,20 @@ class MainActivity : AppCompatActivity() {
         // r.layout.액티비티메인을 이렇게 쓰는 것
         setContentView(binding.root)
 
+        // 액티비티 생명주기 구성 변경 발생 막는 법, 간단하고 가벼운 ui 상태 저장
+        // 이전의 상태를 저장해놨다가 다시 불러오는 것
+        // 이걸 불러오려면 onSaveInstanceState 함수를 오버라이드해서 사용할 수 있음
+        if(savedInstanceState != null)
+            count=savedInstanceState.getInt("count",0)
+
         // 리스너 호출 함수
         setupListeners()
+        updateCountDisplay() // 다시 불러들일때 업데이트해줘야 화면에 바뀌게 됨
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("count",count) // putInt 정수로 집어넣기
     }
 
     private fun setupListeners() {
@@ -57,6 +69,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // 실제 프론트엔드 화면에 적용시켜주는 함수
     private fun updateCountDisplay(){
         binding.textViewCount.text=count.toString()
 
